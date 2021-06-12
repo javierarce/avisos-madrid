218fa52de73cecc8f01d6515bbf5bcad74c0d128
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-06-12 22:05:07 +0200

    Code refactor

---
+++ js/items.js
@@ -0,0 +1,65 @@
+class Items {
+  constructor (map) {
+    this.map = map
+    this.$currentItem = undefined
+
+    let URL = `output.json?${Math.random() * 1000}`
+    let headers = { 'Content-Type': "application/json" }
+
+    fetch(URL, { headers })
+      .then(res => res.json())
+      .then(this.onGetData.bind(this))
+
+  }
+
+  createItem (name, data) {
+    let $el = document.createElement('div')
+
+    if (data) {
+      $el.innerHTML = data
+    }
+
+    $el.classList.add(`Item${name ? '__' + name : '' }`)
+
+    return $el
+  }
+
+  renderItem (item) {
+    let $service = this.createItem('service', item.service)
+    let $footer = this.createItem('footer')
+
+    let time = timeSince(new Date(item.date))
+    let $metadata = this.createItem('metadata', `${time} en ${item.address}`)
+
+    let content = `<p>${item.description}</p>`
+
+    if (item.url) {
+      content += `<img src="${item.url}" />`
+    } 
+
+    let $item = this.createItem(undefined, content)
+
+    $item.appendChild($footer)
+    $footer.appendChild($service)
+    $footer.appendChild($metadata)
+
+    this.$el.appendChild($item)
+
+    $item.onclick = () => {
+      if (this.$currentItem) {
+       this.$currentItem.classList.remove('is-active')
+      }
+
+      $item.classList.add('is-active')
+      this.map.visit(item.lat, item.long)
+      this.$currentItem = $item
+    }
+  }
+
+  onGetData (json) {
+    this.$el = document.body.querySelector('.Items')
+    json.data.forEach(this.renderItem.bind(this))
+  }
+
+}
+
---
+++ js/main.js
@@ -0,0 +1,55 @@
+const timeSince  = (date) => {
+  let seconds = Math.floor((new Date() - date) / 1000)
+
+  let interval = seconds / 31536000
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'año' : 'años'
+    return `Hace ${amount} ${unit}`
+  }
+
+  interval = seconds / 2592000
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'mes' : 'meses'
+    return `Hace ${amount} ${unit}`
+  }
+
+  interval = seconds / 86400
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'día' : 'días'
+    return `Hace ${amount} ${unit}`
+  }
+
+  interval = seconds / 3600
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'h' : 'h'
+    return `Hace ${amount}${unit}`
+  }
+
+  interval = seconds / 60
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'm' : 'm'
+    return `Hace ${amount}${unit}`
+  }
+
+  let amount = Math.floor(seconds)
+  let unit = amount === 1 ? 'segundo' : 'segundos'
+
+  return amount < 5 ? 'ahora' : `Hace ${amount} ${unit}`
+}
+
+const onLoad = () => {
+  map = new Map()
+  items = new Items(map)
+}
+
+window.onload = onLoad
