6364d783c52b7f74d9f08f9b3380a8e001103a5b
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-06-12 12:25:28 +0200

    Adds date and address

---
+++ main.js
@@ -0,0 +1,88 @@
+const timeSince  = (date) => {
+  let seconds = Math.floor((new Date() - date) / 1000)
+ 
+  let interval = seconds / 31536000
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'year' : 'years'
+    return `${amount} ${unit} ago`
+  }
+
+  interval = seconds / 2592000
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'month' : 'months'
+    return `${amount} ${unit} ago`
+  }
+
+  interval = seconds / 86400
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'day' : 'days'
+    return `${amount} ${unit} ago`
+  }
+
+  interval = seconds / 3600
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'hour' : 'hours'
+    return `${amount} ${unit} ago`
+  }
+
+  interval = seconds / 60
+
+  if (interval > 1) {
+    let amount = Math.floor(interval)
+    let unit = amount === 1 ? 'minute' : 'minutes'
+    return `${amount} ${unit} ago`
+  }
+
+  let amount = Math.floor(seconds)
+  let unit = amount === 1 ? 'second' : 'seconds'
+
+  return amount < 5 ? 'now' : `${amount} ${unit} ago`
+}
+
+const onLoad = () => {
+
+  let URL = 'output.json'
+  let headers = { 'Content-Type': "application/json" }
+
+  fetch(URL, { headers })
+    .then(res => res.json())
+    .then((json) => {
+      let $content = document.body.querySelector('.Content')
+
+      json.data.forEach((item) => {
+
+        let date = new Date(item.date)
+        let $date = document.createElement('div')
+        $date.innerHTML = timeSince(date)
+        $date.classList.add('Item__date')
+
+        let $address = document.createElement('div')
+        $address.innerHTML = item.address
+        $address.classList.add('Item__address')
+
+        let $footer = document.createElement('div')
+        $footer.classList.add('Item__footer')
+
+        let $item = document.createElement('div')
+
+        $item.classList.add('Item')
+        $item.innerHTML = `<p>${item.description}</p>`
+        $item.appendChild($footer)
+        $footer.appendChild($date)
+        $footer.appendChild($address)
+
+        $content.appendChild($item)
+
+      })
+    })
+}
+
+window.onload = onLoad
