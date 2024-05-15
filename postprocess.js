376a00b28cb1c54c032ad369ed54af510200a397
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-05-15 11:56:55 +0200

    Limit to 100 messages

--- postprocess.js
+++ postprocess.js
@@ -4,31 +4,36 @@ const filename = Deno.args[0] || 'input.json'
 
 const messages = await readJSON(filename)
 const output = await readJSON('output.json') 
+const backup = await readJSON('backup.json') 
 
-let data = []
+let backupData = []
+const data = []
 
 const lastTime = new Date(output.time).getTime()
 
 messages.forEach((input) => {
-  let id = input.token
-  let date = input.requested_datetime
-  let itemDate = new Date(date).getTime()
+  const id = input.token
+  const date = input.requested_datetime
+  const itemDate = new Date(date).getTime()
+
+  const long = input.long
+  const lat = input.lat
+  const address = input.address
+  const supporting = input.supporting
+  const complaining = input.complaining
+  const description  = input.description
+  const typology = input.typology.typology_description
+  const service = input.service_name
+  const url = input.media_url
+
+  data.push({ id, date , long, lat, address, supporting, complaining, description, service, typology, url })
 
   if (itemDate > lastTime) {
-    let long = input.long
-    let lat = input.lat
-    let address = input.address
-    let supporting = input.supporting
-    let complaining = input.complaining
-    let description  = input.description
-    let typology = input.typology.typology_description
-    let service = input.service_name
-    let url = input.media_url
-
-    data.push({ id, date , long, lat, address, supporting, complaining, description, service, typology, url })
+    backupData.push({ id, date , long, lat, address, supporting, complaining, description, service, typology, url })
   }
 })
 
-data = data.concat(output.data)
-let time = data[0].date
+backupData = backupData.concat(backup.data)
+const time = data[0].date
+await writeJSON('backup.json', { time, data:backupData })
 await writeJSON('output.json', { time, data })
