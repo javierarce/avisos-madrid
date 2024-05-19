0e79c7d19765e501783ed6b51be85fbfa1f2fa7a
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-05-19 21:15:22 +0200

    Updates gen script

--- generate_html.ts
+++ generate_html.ts
@@ -10,25 +10,49 @@ async function generateHTML() {
   for (const message of messages.data) {
     let content = `
     <!DOCTYPE html>
-    <html lang="en">
+    <html lang="es">
     <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="@YourTwitterHandle">
-    <meta name="twitter:title" content="${message.description.slice(0, 70)}">
-    <meta name="twitter:description" content="${message.description}">
-    <meta name="twitter:image" content="${message.url}">
     <title>${message.description.slice(0, 50)}</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="Javier Arce" />
+    <meta name="description" content="${message.description}">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="twitter:image" content="${message.url || 'https://javierarce.github.io/avisos-madrid/card.png'}">
+    <meta property='og:image' content="${message.url || 'https://javierarce.github.io/avisos-madrid/card.png'}"/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="theme-color" content="#FEF6F0">
+    <meta property="og:title" content="${message.description.slice(0, 50)}" />
+    <meta property="og:site_name" content="Avisos Madrid" />
+    <meta name="twitter:title" content="${message.description.slice(0, 70)}">
+    <meta property="og:description" content="${message.description}" />
+    <meta property="twitter:description" content="${message.description}" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.0/dist/leaflet.css" />
+    <link rel="stylesheet" href="../css/style.css" />
+    <script src="https://unpkg.com/leaflet@1.7.0/dist/leaflet.js"></script>
+    <script src="../js/Map.js"></script>
+    <script src="../js/Pagination.js"></script>
+    <script src="../js/Items.js"></script>
+    <script src="../js/main.js"></script>
     </head>
     <body>
-    <h1>${message.description}</h1>
-    <p>Date: ${message.date}</p>
-    `
+    <div class="Content">
+      <div class="Posts">
+        <h1>${message.description}</h1>
+        <p>Date: ${message.date}</p>`;
+        
     if (message.url) {
-      content += `<img src="${message.url}" alt="Image">`
-    } 
-    content += `</body></html>`;
+      content += `<img src="${message.url}" alt="Image">`;
+    }
+    content += `
+        <div class="Footer">
+          Origen de los datos: Ayuntamiento de Madrid.
+        </div>
+      </div>
+    </div>
+    </body>
+    </html>`;
+    
     await Deno.writeTextFile(`./docs/messages/${message.id}.html`, content);
   }
 }
